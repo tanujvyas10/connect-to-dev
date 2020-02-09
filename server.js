01f893a248692f0c5215f6cea5e9175e2cb1962b
tanuj <tanujvyas10@gmail.com>
2020-02-10 00:54:13 +0530

    first

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+const users = require("./routes/api/user")
+const posts = require("./routes/api/posts")
+const profile = require("./routes/api/profile")
+
+const app = express()
+
+
+
+//db config
+
+const db= require('./config/keys').mongoURI;
+
+//connect to mongodb
+
+mongoose.connect(db).then(()=> console.log("mongodb is connected"))
+.catch(err=> console.log(err))
+
+app.get('/',(req,res)=>{
+    res.send("hrllo")
+})
+
+//use routes
+app.use("/api/users",users)
+app.use("/api/profile",profile)
+app.use("/api/posts",posts)
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>console.log(`server running on port ${port}`))
