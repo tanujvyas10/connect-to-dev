a5028fa5fec3eb3cf7e731ddb612171996cb717b
tanuj <tanujvyas10@gmail.com>
2020-03-01 00:53:18 +0530

    added the redux basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import { Provider } from 'react-redux';
 
+
+import store from './store'
 import './App.css'; 
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/footer'
@@ -8,9 +11,12 @@ import Landing from './components/layouts/Landing'
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+
+
 class App extends Component {
 render(){
   return (
+    <Provider store = {store}>
     <Router>
     <div className="App">
     <Navbar/>
@@ -23,6 +29,7 @@ render(){
      <Footer/>
      </div>
     </Router>
+    </Provider>
    
   );
 }
